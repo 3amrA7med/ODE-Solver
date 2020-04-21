12934b1beceffb7d55247a48478e3d1ac51cc73e
AhmedAfifi98 <Ahmed.Afifi98@eng-st.cu.edu.eg>
2020-04-21 03:55:30 +0200

    Fix Initialisation Problem in I/O Module

--- IO Module/Decoder_Receiver.v
+++ IO Module/Decoder_Receiver.v
@@ -4,7 +4,7 @@ module Decoder_Receiver #(parameter ADDRESS_WIDTH = 13,
                           parameter DATA_WIDTH = 64)
                          (input RST,
                           input CLK,
-                          input [31 :0] CPU_Bus,
+                          output [31 :0] CPU_Bus,
                           input Loading_Enable,           			// Module Enable Signal
                           output reg Done_Loading,        			// Module Done Signal
                           output reg Done_Processing_Current_Packet,  		// Handshaking with CPU
@@ -31,8 +31,10 @@ module Decoder_Receiver #(parameter ADDRESS_WIDTH = 13,
 
     // Two Registers To Store The Packet Size & Number of Rows in Input Data
     // [The 1st Two Received Data From CPU]
-    wire [5 :0] PacketSize = (InitCount == 0) ? CPU_Bus[5: 0] : PacketSize;
-    wire [15 :0] RowsNum   = (InitCount == 1) ? CPU_Bus[15: 0] : RowsNum;
+    wire [5 :0] PacketSize = (InitCount < 0) ? {6 {1'b0}} : 
+			     (InitCount == 0) ? CPU_Bus[5: 0] : PacketSize;
+    wire [15 :0] RowsNum   = (InitCount < 1) ? {16 {1'b0}} : 
+			     (InitCount == 1) ? CPU_Bus[15: 0] : RowsNum;
 
     // Counter to Store Number of Processed Rows
     wire [15 :0] RowsCount;
@@ -56,10 +58,12 @@ module Decoder_Receiver #(parameter ADDRESS_WIDTH = 13,
         Done_Element = 0;
 	Done_Loading = 0;
 	Row_Last_Bit = 0;
+	Row_Done_Bit = 0;
 	Writing_Size = 0;
 	RAM_Address  = -1;
 	N_Indication_Bit = 0;
 	Writing_Last_Index = 0;
+	Real_Loading_Enable = 0;
         Small_Packet_Data_Size = 0;
 	Update_Address_Indication_Bit = 0;
 	Decoded_Data = {DATA_WIDTH {1'b0}};
@@ -67,7 +71,7 @@ module Decoder_Receiver #(parameter ADDRESS_WIDTH = 13,
 	Done_Processing_Current_Packet  = 1;
         Writing_Start_Index = DATA_WIDTH - 1;
 	Relative_Address = X0_STARTING_ADDRESS;
-	Small_Packet_Indication_Bit_Location = 0;
+	Small_Packet_Indication_Bit_Location = {6 {1'b0}};
     end
    always @(posedge CLK) begin
 	//  Delay Loading Enable for One Clock Cycle
@@ -78,8 +82,13 @@ module Decoder_Receiver #(parameter ADDRESS_WIDTH = 13,
 
     integer i = 0;
     always @(posedge CLK) begin
-
-        Start_Bit = (InitCount == 2);
+	if(RST) begin
+		Start_Bit = 0;
+	    	Row_Done_Bit = 0;
+		Small_Packet_Indication_Bit_Location = {6 {1'b0}};
+	end
+	else begin
+        	Start_Bit = (InitCount == 2);
 //--------------------------------- The Main Procedure --------------------------------------------------------//
 // It Starts after Reading the Data Required For Decoding [Packet Size & Number Of Rows]-----------------------//
 // It Works untill Reading All Rows => Done Loading -----------------------------------------------------------//
@@ -87,113 +96,114 @@ module Decoder_Receiver #(parameter ADDRESS_WIDTH = 13,
 // 1) Writing Data To Memory In Case there's an Element that has been Decoded Successfully => Done_Element ----//
 // 2) In case the Decoded Data < DATA_WIDTH, Start To Process Another Small Packet => Not Done_Element --------//
 //-------------------------------------------------------------------------------------------------------------//
-        if (Start_Bit && ~Done_Loading) begin
+        	if (Start_Bit && ~Done_Loading) begin
 
-	    // Done Element Delayed as I Want to stall the Next Packet not The Current One
-	    if(~Done_Element_Delayed) 
-		// IF Reached The Last Small Packet in The 32 bits => Start from the Beginning Again
-		if (Small_Packet_Indication_Bit_Location == PacketSize - 1)
-			Small_Packet_Indication_Bit_Location = 31;
-		// Go To The Next Packet
-		else 
-			Small_Packet_Indication_Bit_Location = Small_Packet_Indication_Bit_Location - PacketSize; 
+	    	// Done Element Delayed as I Want to stall the Next Packet not The Current One
+	    	if(~Done_Element_Delayed) 
+			// IF Reached The Last Small Packet in The 32 bits => Start from the Beginning Again
+			if (Small_Packet_Indication_Bit_Location == PacketSize - 1)
+				Small_Packet_Indication_Bit_Location = 31;
+			// Go To The Next Packet
+			else 
+				Small_Packet_Indication_Bit_Location = Small_Packet_Indication_Bit_Location - PacketSize; 
 
-	    // This Bit is used to Store the Last Bit of The First Packet in the Row, We'll Compare this Bit
-	    // to all Last Bits of the next Packets till It Changes which means => This is the Last Packet in the Row
-	    // Row_Done_Bit_Delayed as  want to change it to the Last Bit of the First Packet in the next Row So I Delayed Row_Done_Bit one Cycle
-	    Row_Last_Bit = Row_Done_Bit_Delayed ? CPU_Bus[Small_Packet_Indication_Bit_Location] : Row_Last_Bit;
+	    	// This Bit is used to Store the Last Bit of The First Packet in the Row, We'll Compare this Bit
+	    	// to all Last Bits of the next Packets till It Changes which means => This is the Last Packet in the Row
+	    	// Row_Done_Bit_Delayed as  want to change it to the Last Bit of the First Packet in the next Row So I Delayed Row_Done_Bit one Cycle
+	    	Row_Last_Bit = Row_Done_Bit_Delayed ? CPU_Bus[Small_Packet_Indication_Bit_Location] : Row_Last_Bit;
 
-	    // This Bit Indicate The Last Packet in The Row using The Previous Criteria 
-	    // It's Anded with Its Inverse to Make It High for only one cycle 
-            Row_Done_Bit = (CPU_Bus[Small_Packet_Indication_Bit_Location] != Row_Last_Bit) && ~Row_Done_Bit;
+	    	// This Bit Indicate The Last Packet in The Row using The Previous Criteria 
+	    	// It's Anded with Its Inverse to Make It High for only one cycle 
+           	 Row_Done_Bit = (CPU_Bus[Small_Packet_Indication_Bit_Location] != Row_Last_Bit) && ~Row_Done_Bit;
 		
-	    // Bit Indicating The End of The Process when All Rows are Processed
-	    Done_Loading = (RowsCount == RowsNum);
+	    	// Bit Indicating The End of The Process when All Rows are Processed
+	   	 Done_Loading = (RowsCount == RowsNum);
 
-	   // This is the Handshaking Signal Sent to the CPU to Send Another 32 Bits
-	    Done_Processing_Current_Packet  = (Small_Packet_Indication_Bit_Location == PacketSize - 1) && ~Done_Element;
+	   	// This is the Handshaking Signal Sent to the CPU to Send Another 32 Bits
+	   	 Done_Processing_Current_Packet  = (Small_Packet_Indication_Bit_Location == PacketSize - 1) && ~Done_Element;
 
-	    // This Bit is used To Indicate That Some Row is Done But It'll be High till the First Element in the Next Row
-	    // is calculated and Ready to be written in Memory so I Can Change The Address That It'll be Written at.
-	    Update_Address_Indication_Bit =  Row_Done_Bit_Delayed || Update_Address_Indication_Bit;
+	    	// This Bit is used To Indicate That Some Row is Done But It'll be High till the First Element in the Next Row
+	    	// is calculated and Ready to be written in Memory so I Can Change The Address That It'll be Written at.
+	   	 Update_Address_Indication_Bit =  Row_Done_Bit_Delayed || Update_Address_Indication_Bit;
 //---------------------------------------------------------------------------------------------------------	
-	    // Operation 2) => Process The Packet     
-            if (~Done_Element) begin
-		// Get The Packet Size => Number Of Consecutive Zeros Or Ones
-	        for (i = 0; i < 31; i = i + 1) begin
-		    if(i < PacketSize - 1) begin
-                    	Small_Packet_Data_Size[PacketSize - i - 2] =  CPU_Bus[Small_Packet_Indication_Bit_Location - i - 1];
-		    end
-                end
-		// Data Size Represents Number of Already Decoded Bits + This Packet Size 
-		// This Variable is the Indicator of Operation 1 or 2 
-                Data_Size = Data_Size + Small_Packet_Data_Size;
+	    	// Operation 2) => Process The Packet     
+            	if (~Done_Element) begin
+			// Get The Packet Size => Number Of Consecutive Zeros Or Ones
+	       		 for (i = 0; i < 31; i = i + 1) begin
+		    		if(i < PacketSize - 1) begin
+                    			Small_Packet_Data_Size[PacketSize - i - 2] =  CPU_Bus[Small_Packet_Indication_Bit_Location - i - 1];
+		   		 end
+                	end
+			// Data Size Represents Number of Already Decoded Bits + This Packet Size 
+			// This Variable is the Indicator of Operation 1 or 2 
+                	Data_Size = Data_Size + Small_Packet_Data_Size;
 
-		// Bit to Indicate If This Packet Represent Zero Or One 
-		// It Alternates Each Clock Cycle of Operation 2 
-		// It initialised with the Last Bit of the First Packet For Each Row
-                Data_Bit  = Row_Done_Bit_Delayed ? Row_Last_Bit : ~Data_Bit;
-            end
+			// Bit to Indicate If This Packet Represent Zero Or One 
+			// It Alternates Each Clock Cycle of Operation 2 
+			// It initialised with the Last Bit of the First Packet For Each Row
+               		 Data_Bit  = Row_Done_Bit_Delayed ? Row_Last_Bit : ~Data_Bit;
+            	end
 //---------------------------------------------------------------------------------------------------------
-	    // If Data_Size >= DATA_WIDTH => Operation 2 => Write Elements in Memory
-	    // If Data_Size < DATA_WIDTH => Operation 1 => Process another Packet
-	    Writing_Size = (Data_Size < DATA_WIDTH) ? Data_Size - (DATA_WIDTH - 1 - Writing_Start_Index) : Writing_Size;
-	    Writing_Last_Index = (Data_Size < DATA_WIDTH) ? Writing_Start_Index - Writing_Size + 1 : 0;
-	    // Complete The Decoded Data based on Writing Start Index and Writing Last Index
-            for (i = DATA_WIDTH - 1; i >= 0; i = i - 1) begin
-		if (i <= Writing_Start_Index && i >= Writing_Last_Index) begin
-                    Decoded_Data[i] = Data_Bit;
-		end
-            end
-	    // The Writing Start Index will Return to the First Bit in The Decoded Data if Data_Size was >= 64
-	    // But if Data Size < 64 It'll be Equal Itself - Writing Size
-            Writing_Start_Index = (Data_Size < DATA_WIDTH) ? Writing_Start_Index - Writing_Size : DATA_WIDTH - 1;
-	    Done_Element = (Data_Size >= DATA_WIDTH);
+	    	 // If Data_Size >= DATA_WIDTH => Operation 2 => Write Elements in Memory
+	   	 // If Data_Size < DATA_WIDTH => Operation 1 => Process another Packet
+	   	 Writing_Size = (Data_Size < DATA_WIDTH) ? Data_Size - (DATA_WIDTH - 1 - Writing_Start_Index) : Writing_Size;
+	    	 Writing_Last_Index = (Data_Size < DATA_WIDTH) ? Writing_Start_Index - Writing_Size + 1 : 0;
+	    	 // Complete The Decoded Data based on Writing Start Index and Writing Last Index
+            	 for (i = DATA_WIDTH - 1; i >= 0; i = i - 1) begin
+			if (i <= Writing_Start_Index && i >= Writing_Last_Index) begin
+                   	 Decoded_Data[i] = Data_Bit;
+			end
+            	 end
+	    	 // The Writing Start Index will Return to the First Bit in The Decoded Data if Data_Size was >= 64
+	   	 // But if Data Size < 64 It'll be Equal Itself - Writing Size
+           	 Writing_Start_Index = (Data_Size < DATA_WIDTH) ? Writing_Start_Index - Writing_Size : DATA_WIDTH - 1;
+	    	 Done_Element = (Data_Size >= DATA_WIDTH);
 //------------------------------ Store The Decoded Element in Memory ---------------------------------------------	
-            if (Data_Size >= DATA_WIDTH) begin
-		// Update Ram Data 
-                RAM_Data = Decoded_Data;
-		// Calculate The RAM Address 
-		// IF Matrix A is Done and This is a New Row => Change To Matrix B Address
-	    	if((RowsCount == N + 1) && Update_Address_Indication_Bit) begin
-			Update_Address_Indication_Bit = 0;
-			RAM_Address = B_STARTING_ADDRESS;	 
-	    	end 
-		// IF Matrix B is Done and This is a New Row => Change To Vector X0 Address
-	    	else if((RowsCount == (N << 1) + 1) && Update_Address_Indication_Bit) begin
-			Update_Address_Indication_Bit = 0;
-			RAM_Address = X0_STARTING_ADDRESS;	 
-	    	end
-		// IF Vector X0 is Done and This is a New Row => Add 50 To the Address for each following vector 
-	    	else if((RowsCount > (N << 1) + 1) && Update_Address_Indication_Bit) begin
-			Update_Address_Indication_Bit = 0;
-			Relative_Address = Relative_Address + 50;
-			RAM_Address = Relative_Address; 
-	   	end
-		// OtherWise Increment the Ram Address By One
-	   	else begin 
-			RAM_Address = RAM_Address + 1;
-	  	end
+            	if (Data_Size >= DATA_WIDTH) begin
+			// Update Ram Data 
+                	RAM_Data = Decoded_Data;
+			// Calculate The RAM Address 
+			// IF Matrix A is Done and This is a New Row => Change To Matrix B Address
+	    		if((RowsCount == N + 1) && Update_Address_Indication_Bit) begin
+				Update_Address_Indication_Bit = 0;
+				RAM_Address = B_STARTING_ADDRESS;	 
+	    		end 
+			// IF Matrix B is Done and This is a New Row => Change To Vector X0 Address
+	    		else if((RowsCount == (N << 1) + 1) && Update_Address_Indication_Bit) begin
+				Update_Address_Indication_Bit = 0;
+				RAM_Address = X0_STARTING_ADDRESS;	 
+	    		end
+			// IF Vector X0 is Done and This is a New Row => Add 50 To the Address for each following vector 
+	    		else if((RowsCount > (N << 1) + 1) && Update_Address_Indication_Bit) begin
+				Update_Address_Indication_Bit = 0;
+				Relative_Address = Relative_Address + 50;
+				RAM_Address = Relative_Address; 
+	   		end
+			// OtherWise Increment the Ram Address By One
+	   		else begin 
+				RAM_Address = RAM_Address + 1;
+	  		end
 //---------------------------------------------------------------------------------------------------------
-		// I Want To Read The N Value as It's used in RAM Address Calculations
-		// So I need a Bit to indicate That This Element is The First Done Element which is N
-		// So It won't be overrided By the Following Elements 
-		if(N_Indication_Bit == 0) begin
-		    N_Indication_Bit = 1;
-                    N = Decoded_Data[11:0];
-		end	    
-            end
-	    // Remove The Decoded Data From The Data Size [It'll Be Used in The Next Clock Cycle]
-	    // as We may Write More Than one Element 
-	    Data_Size = (Data_Size >= DATA_WIDTH) ? Data_Size - DATA_WIDTH: Data_Size;
-        end
+			// I Want To Read The N Value as It's used in RAM Address Calculations
+			// So I need a Bit to indicate That This Element is The First Done Element which is N
+			// So It won't be overrided By the Following Elements 
+			if(N_Indication_Bit == 0) begin
+		    		N_Indication_Bit = 1;
+                    		N = Decoded_Data[11:0];
+			end	    
+            	end
+	    	// Remove The Decoded Data From The Data Size [It'll Be Used in The Next Clock Cycle]
+	    	// as We may Write More Than one Element 
+	   	 Data_Size = (Data_Size >= DATA_WIDTH) ? Data_Size - DATA_WIDTH: Data_Size;
+       		 end
 //--------------------------------- The Main Procedure End ----------------------------------------------------//
 //--------------------- Initilization that I want to be Done before starting the Main Procedure --------------//
-	else begin 
-            Row_Done_Bit = (InitCount < 1);
-	    Small_Packet_Indication_Bit_Location = 31 + PacketSize;
-	end
+		else begin 
+            		Row_Done_Bit = (InitCount < 1);
+	    		Small_Packet_Indication_Bit_Location = 31 + PacketSize;
+		end
 //-----------------------------------------------------------------------------------------------------------//
+	end
     end
  endmodule
         
--- IO Module/IO_Module.v
+++ IO Module/IO_Module.v
@@ -7,7 +7,7 @@ module IO_Module #(parameter ADDRESS_WIDTH = 13,
                    inout [31 :0] CPU_Bus,
                    output Done_Loading,
                    output Done_Processing_Current_Packet,
-                   output Done_Processing,
+                   output reg Done_Processing,
                    output reg IO_Memory_WR_Enable,
                    input [DATA_WIDTH - 1 :0] RAM_Data_RD_A,
                    input [DATA_WIDTH - 1 :0] RAM_Data_RD_B,
@@ -16,8 +16,8 @@ module IO_Module #(parameter ADDRESS_WIDTH = 13,
                    output reg [ADDRESS_WIDTH - 1 :0] RAM_Address_RD_B,
                    output reg [ADDRESS_WIDTH - 1 :0] RAM_Address_WR);
     
-    wire Loading_Enable = (INT && Load_Process) || Loading_Enable;
-    wire Sending_Enable = (INT && ~Load_Process) || Sending_Enable;
+    wire Loading_Enable = (INT && Load_Process);
+    wire Sending_Enable;
     wire [ADDRESS_WIDTH - 1 :0] Decoder_Memory_Address_WR;
     wire [ADDRESS_WIDTH - 1 :0] Sender_Memory_Address_RD_A;
     wire [ADDRESS_WIDTH - 1 :0] Sender_Memory_Address_RD_B;
@@ -32,6 +32,15 @@ module IO_Module #(parameter ADDRESS_WIDTH = 13,
     Results_Sender #(ADDRESS_WIDTH ,DATA_WIDTH) RS
     (RST, CLK, Sending_Enable, CPU_Bus, Done_Sending,
     RAM_Data_RD_A, RAM_Data_RD_B, Sender_Memory_Address_RD_A, Sender_Memory_Address_RD_B);
+	
+    initial begin
+		RAM_Address_WR   =  {ADDRESS_WIDTH {1'b0}};
+        RAM_Data_WR      =  {DATA_WIDTH {1'b0}};
+        RAM_Address_RD_A =  {ADDRESS_WIDTH {1'b0}};
+        RAM_Address_RD_B =  {ADDRESS_WIDTH {1'b0}};
+        IO_Memory_WR_Enable = 0;
+		Done_Processing = 0;
+    end
 
     always @* begin
         RAM_Address_WR = Decoder_Memory_Address_WR;
